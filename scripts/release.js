be19a5a0b65845b17441c69a1b641168b2cebd12
Rick Harrison <r@rickharrison.me>
2016-07-20 14:29:51 -0700

    Adds npm publish script.

---
+++ scripts/release.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/* eslint no-console: 0, no-sync: 0, no-process-exit: 0 */
+
+const execSync = require('child_process').execSync;
+const fs = require('fs');
+const packageJson = require('../package.json');
+
+const nextVersion = process.argv[process.argv.length - 1];
+const nextRef = `v${nextVersion}`;
+const semverRegex = /[0-9]+\.[0-9]+\.[0-9](-.+)?/ig;
+
+function validateSemver (version) {
+  if (!semverRegex.test(version)) {
+    console.error(`Version ${version} is not valid! It must be a valid semver string`);
+    process.exit(1);
+  }
+}
+
+validateSemver(nextVersion);
+
+console.log(`Current Version: ${packageJson.version}`);
+console.log(`Next Version: ${nextVersion}`);
+
+packageJson.version = nextVersion;
+
+fs.writeFileSync('package.json', `${JSON.stringify(packageJson, null, 2)}\n`, 'utf8');
+
+execSync(`git commit -am 'Release Version: ${nextRef}'`);
+execSync(`git tag ${nextRef}`);
+execSync('git push origin master');
+execSync(`git push origin ${nextRef}`);
+
+execSync('npm publish', { stdio: [0, 1, 2] });
